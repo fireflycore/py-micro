026229c4ada6d3ef04dd083837f5b695df0dcae8
lhdht <15229684090@163.com>
2026-06-22 08:41:47 +0800

    perfect: add invocation related docs

--- src/firefly/micro/invocation/__init__.py
+++ src/firefly/micro/invocation/__init__.py
@@ -1,13 +1,22 @@
 """Firefly 远程调用公共入口。"""
 
 from .dns import DNS, RemoteServiceRegistry
+from .invoker import InvokeRequest, UnaryInvoker
+from .manager import ConnectionManager, ConnectionManagerOptions
 from .metadata import DEFAULT_INVOKE_TIMEOUT_SECONDS, prepare_metadata_for_invocation
+from .service import RemoteServiceCaller, RemoteServiceManaged
 from .target import Target
 
 __all__ = [
+    "ConnectionManager",
+    "ConnectionManagerOptions",
     "DEFAULT_INVOKE_TIMEOUT_SECONDS",
     "DNS",
+    "InvokeRequest",
     "RemoteServiceRegistry",
+    "RemoteServiceCaller",
+    "RemoteServiceManaged",
     "Target",
+    "UnaryInvoker",
     "prepare_metadata_for_invocation",
 ]
